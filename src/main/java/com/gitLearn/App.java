11788d7b24bc6dc0058e8bebe665e985ac6944ef
lizhe_git <lizhe_git@email>
2022-04-16 14:36:02 +0800

    third commit

--- src/main/java/com/gitLearn/App.java
+++ src/main/java/com/gitLearn/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World! git test" );
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello hot-fix git1");
     }
 }
